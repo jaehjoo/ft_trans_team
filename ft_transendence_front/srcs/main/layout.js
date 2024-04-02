7ea74f65952c722169073514f731f51cd090a3e7
kcwww <cwkim0321@gmail.com>
2024-04-02 17:44:23 +0900

    feat : add useState component

--- ft_transendence_front/srcs/main/layout.js
+++ ft_transendence_front/srcs/main/layout.js
@@ -1,6 +1,8 @@
+import { render } from "../../MyReact";
 import parser from "../../utils/parser";
 
 const Main = (children) => {
+  if (children === undefined) children = "";
   const MainStr = /*html*/ `
     <main class="d-flex flex-column p-2 bg-primary vh-100 justify-content-between align-items-center">
     <div class="d-flex w-100">
@@ -8,23 +10,21 @@ const Main = (children) => {
     </div>
     <section class="w-100 d-flex p-2 bg-light justify-content-between align-items-center gap-2">
       <div id="renderSection" class="d-flex w-50 flex-column gap-2">
+      ${children}
       </div>
       <img src="../../public/img/main.png" alt="main" class="img-fluid w-50">
     </section>
     <footer>Made with by 42 Cadets</footer>
   `;
-
-  const mainElement = parser(MainStr, "main");
-  const renderSection = mainElement.querySelector("#renderSection");
-  renderSection.appendChild(children);
-
-  return mainElement;
+  return MainStr;
 };
 
-const MainLayout = (children) => {
-  const app = document.getElementById("app");
-  app.innerHTML = "";
-  app.appendChild(Main(children));
+const MainLayout = (component) => {
+  const children = component();
+
+  return /*html*/ `
+    ${Main(children)}
+  `;
 };
 
 export default MainLayout;
