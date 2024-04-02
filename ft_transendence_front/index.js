2a6f31c82c3787c0b6b8316afe9cd4472716f893
kcwww <cwkim0321@gmail.com>
2024-04-02 15:00:59 +0900

    feat: parser

--- ft_transendence_front/index.js
+++ ft_transendence_front/index.js
@@ -6,10 +6,9 @@ import LoginPage from "./srcs/login/login";
 import faPage from "./srcs/login/2fa";
 
 const App = () => {
-  // 라우터 처리
-  if (window.location.pathname === "/ui") {
-    renderUIPage();
-  } else if (window.location.pathname === "/2fa") {
+  // // 라우터 처리
+
+  if (window.location.pathname === "/2fa") {
     MainLayout(faPage());
   } else {
     MainLayout(LoginPage());
