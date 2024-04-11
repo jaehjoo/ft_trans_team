b9ec5870e49a0511b3ddd96d65164e0fdb91428b
kcwww <cwkim0321@gmail.com>
2024-04-11 12:24:07 +0900

    fix: game modal options

--- ft_transendence_front/srcs/main/_components/ModalContent.js
+++ ft_transendence_front/srcs/main/_components/ModalContent.js
@@ -3,7 +3,7 @@ import { useState } from "../../../MyReact";
 const ModalContent = () => {
   const handleClick = (text) => {
     console.log(text, "you should remove event listener here");
-    window.location.href = `/${text}`;
+    window.location.href = `/game/${text}`;
   };
 
   window.modalHandleClick = handleClick;
@@ -17,8 +17,9 @@ const ModalContent = () => {
         
       </div>
       <div class="modal-body d-flex flex-column gap-4">
-        <button type="button" class="btn btn-primary w-100" onclick="modalHandleClick('create')">Create Room</button>
-        <button type="button" class="btn btn-primary w-100" onclick="modalHandleClick('join')">Join Room</button>
+        <button type="button" class="btn btn-primary w-100" onclick="modalHandleClick('1v1')">1 VS 1</button>
+        <button type="button" class="btn btn-primary w-100" onclick="modalHandleClick('2v2')">2 VS 2</button>
+        <button type="button" class="btn btn-primary w-100" onclick="modalHandleClick('tournament')">Tournament</button>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
