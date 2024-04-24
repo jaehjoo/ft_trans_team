395c0f66868b844cd0b547b4a2c8811b36f331e9
kcwww <cwkim0321@gmail.com>
2024-04-24 13:57:06 +0900

    feat: api

--- srcs/requirements/front/srcs/main/_components/Friends.js
+++ srcs/requirements/front/srcs/main/_components/Friends.js
@@ -1,6 +1,14 @@
 import Sidebar from "../../../components/ui/Sidebar.js";
 
+const fetchFriends = async () => {
+  const response = await fetch("/api/friends");
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
 const Friends = () => {
+  fetchFriends();
   return /*html*/ `
     ${Sidebar("Friends List", ["Alice", "Bob", "Charlie"])}
 `;
--- srcs/requirements/front/srcs/main/main.js
+++ srcs/requirements/front/srcs/main/main.js
@@ -1,7 +1,17 @@
 import ButtonGroup from "./_components/ButtonGroup.js";
 import Friends from "./_components/Friends.js";
+//$uri?access={access_value}&refresh={refresh_value}
+const mainFetch = async () => {
+  const access = localStorage.getItem("access_token") || "null";
+  const refresh = localStorage.getItem("refresh_token") || "null";
+  const response = await fetch(`/api/main?access=${access}&refresh=${refresh}`);
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
 
 const MainPage = () => {
+  mainFetch();
   return /*html*/ `
   <div class="d-flex flex-column w-100 justify-content-center align-items-center gap-2">
     <div class="d-flex justify-content-center gap-4">
