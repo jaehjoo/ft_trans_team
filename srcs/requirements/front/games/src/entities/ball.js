85c62af44a3ba18f1ee0961e512e32289df1e887
kcwww <cwkim0321@gmail.com>
2024-04-28 15:13:11 +0900

    feat: hz time feat

--- srcs/requirements/front/games/src/entities/ball.js
+++ srcs/requirements/front/games/src/entities/ball.js
@@ -1,50 +1,46 @@
 export class Ball {
-	constructor(view) {
-		this.window = {
-			WIDTH : view.WIDTH,
-			HEIGHT : view.HEIGHT,
-		}
-		this.ball = {
-			RADIUS : view.WIDTH / 100,
-			X : view.WIDTH / 2,
-			Y : view.HEIGHT / 2,
-		}
-		this.velocity = {
-			X : 3,
-			Y : 3,
-		}
-		this.serve = 0;
-		this.ballColor;
-	}
+  constructor(view) {
+    this.window = {
+      WIDTH: view.WIDTH,
+      HEIGHT: view.HEIGHT,
+    };
+    this.ball = {
+      RADIUS: view.WIDTH / 100,
+      X: view.WIDTH / 2,
+      Y: view.HEIGHT / 2,
+    };
+    this.velocity = {
+      X: 3,
+      Y: 3,
+    };
+    this.serve = 0;
+    this.ballColor;
+  }
 
-	init() {
-		this.ball.X = this.window.WIDTH / 2;
-		this.ball.Y = this.window.HEIGHT / 2;
-		if (this.velocity.X < 0)
-			this.velocity.X = -3;
-		else
-			this.velocity.X = 3;
-		if (this.velocity.Y < 0)
-			this.velocity.Y = -3;
-		else
-			this.velocity.Y = 3;
-		if (this.serve == 2) {
-			this.velocity.X *= -1;
-			this.velocity.Y *= -1;
-			this.serve = 0;
-		}
-	}
+  init() {
+    this.ball.X = this.window.WIDTH / 2;
+    this.ball.Y = this.window.HEIGHT / 2;
+    if (this.velocity.X < 0) this.velocity.X = -3;
+    else this.velocity.X = 3;
+    if (this.velocity.Y < 0) this.velocity.Y = -3;
+    else this.velocity.Y = 3;
+    if (this.serve == 2) {
+      this.velocity.X *= -1;
+      this.velocity.Y *= -1;
+      this.serve = 0;
+    }
+  }
 
-	update() {
-		this.ball.X += this.velocity.X;
-		this.ball.Y += this.velocity.Y;
-	}
+  update(time) {
+    this.ball.X += this.velocity.X * time * 60;
+    this.ball.Y += this.velocity.Y * time * 60;
+  }
 
-	draw(context) {
-		context.beginPath();
-		context.arc(this.ball.X, this.ball.Y, this.ball.RADIUS, 0, Math.PI * 2);
-		context.fillStyle = '#0095DD';
-		context.fill();
-		context.closePath();
-	}
+  draw(context) {
+    context.beginPath();
+    context.arc(this.ball.X, this.ball.Y, this.ball.RADIUS, 0, Math.PI * 2);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+  }
 }
--- srcs/requirements/front/games/src/index.js
+++ srcs/requirements/front/games/src/index.js
@@ -1,253 +1,262 @@
-import { Stage } from "./entities/stage.js"
-import { Ball } from "./entities/ball.js"
-import { Board } from "./entities/board.js"
-import { PlayerOne } from "./entities/fighters/playerOne.js"
-import { PlayerTwo } from "./entities/fighters/playerTwo.js"
-import { Wait } from "./entities/wait.js"
-import { Result } from "./entities/res.js"
+import { Stage } from "./entities/stage.js";
+import { Ball } from "./entities/ball.js";
+import { Board } from "./entities/board.js";
+import { PlayerOne } from "./entities/fighters/playerOne.js";
+import { PlayerTwo } from "./entities/fighters/playerTwo.js";
+import { Wait } from "./entities/wait.js";
+import { Result } from "./entities/res.js";
 
 const GameViewport = {
-	WIDTH : 1024,
-	HEIGHT : 768,
-}
+  WIDTH: 1024,
+  HEIGHT: 768,
+};
 
-window.addEventListener('load', function() {
-	const canvasEl = document.querySelector('canvas');
-	const context = canvasEl.getContext('2d');
+window.addEventListener("load", function () {
+  const canvasEl = document.querySelector("canvas");
+  const context = canvasEl.getContext("2d");
 
-	canvasEl.width = GameViewport.WIDTH;
-	canvasEl.height = GameViewport.HEIGHT;
+  canvasEl.width = GameViewport.WIDTH;
+  canvasEl.height = GameViewport.HEIGHT;
 
-	const entities = [
-		new Stage(GameViewport),
-		new Board(GameViewport),
-		new Ball(GameViewport),
-		new PlayerOne(GameViewport),
-		new PlayerTwo(GameViewport),
-	]
+  const entities = [
+    new Stage(GameViewport),
+    new Board(GameViewport),
+    new Ball(GameViewport),
+    new PlayerOne(GameViewport),
+    new PlayerTwo(GameViewport),
+  ];
 
-	const scene = [
-		new Wait(GameViewport),
-		new Result(GameViewport),
-	]
+  const scene = [new Wait(GameViewport), new Result(GameViewport)];
 
-	let flag = {
-		START : false,
-		STOP : false,
-	}
+  let flag = {
+    START: false,
+    STOP: false,
+  };
 
-	const ws = new WebSocket("wss://" + window.location.host + "/ws/game/practice");
+  const ws = new WebSocket(
+    "wss://" + window.location.host + "/ws/game/practice"
+  );
 
-	let yourName = "";
+  let yourName = "";
 
-	let previousTime = 0
-	let secondsPassed = 0
+  let previousTime = 0;
+  let secondsPassed = 0;
 
-	function sceneDraw() {
-		context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
-		if (flag.START == false) {
-			scene[0].draw(context);
-		}
-		else if (flag.STOP == true) {
-			scene[1].draw(context);
-		}
-	}
+  function sceneDraw() {
+    context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+    if (flag.START == false) {
+      scene[0].draw(context);
+    } else if (flag.STOP == true) {
+      scene[1].draw(context);
+    }
+  }
 
-	function playingDraw() {
-		context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
-		for (const entity of entities) {
-			entity.draw(context);
-		}
-	}
+  function playingDraw() {
+    context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+    for (const entity of entities) {
+      entity.draw(context);
+    }
+  }
 
-	function checkWebSocketMessage() {
-		ws.onmessage = (msg) => {
-			let textData = JSON.parse(msg.data);
-			if (textData.data.mode == "connect") {
-				yourName = textData.data.name;
-			}
-			else if (textData.data.mode == "set.game") {
-				entities[3].name = textData.data.player0;
-				entities[4].name = textData.data.player1;
-				ws.send(
-					JSON.stringify({
-						type: 'set.game',
-						data: {
-							player0: entities[3].name,
-							player1: entities[4].name,
-						}
-					})
-				)
-			}
-			else if (textData.data.mode == "game.start") {
-				flag.START = true;
-			}
-			else if (textData.data.mode == "abnormal.termination") {
-				ws.send(
-					JSON.stringify({
-						type: 'game.clear',
-						data: {
-							"name" : yourName,
-						}
-					})
-				)
-				ws.onclose()
-			}
-		}
-	}
+  function checkWebSocketMessage() {
+    ws.onmessage = (msg) => {
+      let textData = JSON.parse(msg.data);
+      if (textData.data.mode == "connect") {
+        yourName = textData.data.name;
+      } else if (textData.data.mode == "set.game") {
+        entities[3].name = textData.data.player0;
+        entities[4].name = textData.data.player1;
+        ws.send(
+          JSON.stringify({
+            type: "set.game",
+            data: {
+              player0: entities[3].name,
+              player1: entities[4].name,
+            },
+          })
+        );
+      } else if (textData.data.mode == "game.start") {
+        flag.START = true;
+      } else if (textData.data.mode == "abnormal.termination") {
+        ws.send(
+          JSON.stringify({
+            type: "game.clear",
+            data: {
+              name: yourName,
+            },
+          })
+        );
+        ws.onclose();
+      }
+    };
+  }
 
-	function updateBar() {
-		ws.onmessage = (msg) => {
-			let textData = JSON.parse(msg.data);
-			if (textData.data.mode == "update.bar"
-				&& textData.data['name'] != yourName) {
-				if (yourName != entities[3].name) {
-					entities[3].bar.X = textData.data['bar']['x']
-					entities[3].bar.Y = textData.data['bar']['y']
-				}
-				else {
-					entities[4].bar.X = textData.data['bar']['x']
-					entities[4].bar.Y = textData.data['bar']['y']
-				}
-			}
-		}
-	}
+  function updateBar() {
+    ws.onmessage = (msg) => {
+      let textData = JSON.parse(msg.data);
+      if (
+        textData.data.mode == "update.bar" &&
+        textData.data["name"] != yourName
+      ) {
+        if (yourName != entities[3].name) {
+          entities[3].bar.X = textData.data["bar"]["x"];
+          entities[3].bar.Y = textData.data["bar"]["y"];
+        } else {
+          entities[4].bar.X = textData.data["bar"]["x"];
+          entities[4].bar.Y = textData.data["bar"]["y"];
+        }
+      }
+    };
+  }
 
-	function checkWallCollision() {
-		if (entities[2].ball.Y + entities[2].velocity.Y > entities[0].background.HEIGHT - entities[0].borderLine.WIDTH - entities[2].ball.RADIUS
-			|| entities[2].ball.Y + entities[2].velocity.Y < entities[0].borderLine.WIDTH + entities[2].ball.RADIUS)
-			entities[2].velocity.Y *= -1
-		if (entities[3].bar.Y < entities[0].borderLine.WIDTH)
-			entities[3].bar.Y = entities[0].borderLine.WIDTH
-		if (entities[3].bar.Y > entities[0].background.HEIGHT - entities[0].borderLine.WIDTH - entities[3].bar.HEIGHT)
-			entities[3].bar.Y = entities[0].background.HEIGHT - entities[0].borderLine.WIDTH - entities[3].bar.HEIGHT
-		if (entities[4].bar.Y < entities[0].borderLine.WIDTH)
-			entities[4].bar.Y = entities[0].borderLine.WIDTH
-		if (entities[4].bar.Y > entities[0].background.HEIGHT - entities[0].borderLine.WIDTH - entities[4].bar.HEIGHT)
-			entities[4].bar.Y = entities[0].background.HEIGHT - entities[0].borderLine.WIDTH - entities[4].bar.HEIGHT
-	}
+  function checkWallCollision() {
+    if (
+      entities[2].ball.Y + entities[2].velocity.Y >
+        entities[0].background.HEIGHT -
+          entities[0].borderLine.WIDTH -
+          entities[2].ball.RADIUS ||
+      entities[2].ball.Y + entities[2].velocity.Y <
+        entities[0].borderLine.WIDTH + entities[2].ball.RADIUS
+    )
+      entities[2].velocity.Y *= -1;
+    if (entities[3].bar.Y < entities[0].borderLine.WIDTH)
+      entities[3].bar.Y = entities[0].borderLine.WIDTH;
+    if (
+      entities[3].bar.Y >
+      entities[0].background.HEIGHT -
+        entities[0].borderLine.WIDTH -
+        entities[3].bar.HEIGHT
+    )
+      entities[3].bar.Y =
+        entities[0].background.HEIGHT -
+        entities[0].borderLine.WIDTH -
+        entities[3].bar.HEIGHT;
+    if (entities[4].bar.Y < entities[0].borderLine.WIDTH)
+      entities[4].bar.Y = entities[0].borderLine.WIDTH;
+    if (
+      entities[4].bar.Y >
+      entities[0].background.HEIGHT -
+        entities[0].borderLine.WIDTH -
+        entities[4].bar.HEIGHT
+    )
+      entities[4].bar.Y =
+        entities[0].background.HEIGHT -
+        entities[0].borderLine.WIDTH -
+        entities[4].bar.HEIGHT;
+  }
 
-	function checkBarCollision() {
-		if (entities[2].ball.X - entities[2].ball.RADIUS < entities[3].bar.X + 10
-			&& entities[2].ball.Y + entities[2].velocity.Y >= entities[3].bar.Y
-			&& entities[2].ball.Y + entities[2].velocity.Y <= entities[3].bar.Y + entities[3].bar.HEIGHT)
-			entities[2].velocity.X *= -1.1;
-		if (entities[2].ball.X + entities[2].ball.RADIUS > entities[4].bar.X + entities[4].bar.WIDTH - 10
-			&& entities[2].ball.Y + entities[2].velocity.Y >= entities[4].bar.Y
-			&& entities[2].ball.Y + entities[2].velocity.Y <= entities[4].bar.Y + entities[4].bar.HEIGHT)
-			entities[2].velocity.X *= -1.1;
-	}
+  function checkBarCollision() {
+    if (
+      entities[2].ball.X - entities[2].ball.RADIUS < entities[3].bar.X + 10 &&
+      entities[2].ball.Y + entities[2].velocity.Y >= entities[3].bar.Y &&
+      entities[2].ball.Y + entities[2].velocity.Y <=
+        entities[3].bar.Y + entities[3].bar.HEIGHT
+    )
+      entities[2].velocity.X *= -1.1;
+    if (
+      entities[2].ball.X + entities[2].ball.RADIUS >
+        entities[4].bar.X + entities[4].bar.WIDTH - 10 &&
+      entities[2].ball.Y + entities[2].velocity.Y >= entities[4].bar.Y &&
+      entities[2].ball.Y + entities[2].velocity.Y <=
+        entities[4].bar.Y + entities[4].bar.HEIGHT
+    )
+      entities[2].velocity.X *= -1.1;
+  }
 
-	function checkBoundary() {
-		let win;
-		if (entities[2].ball.X < entities[3].bar.X) {
-			entities[2].serve += 1
-			entities[2].init()
-			entities[1].changeScore(1)
-		}
-		if (entities[2].ball.X > entities[4].bar.X + entities[4].bar.WIDTH) {
-			entities[2].serve += 1
-			entities[2].init()
-			entities[1].changeScore(0)
-		}
-		entities[1].checkDuce()
-		win = entities[1].checkWin()
-		if (win) {
-			let player;
-			if (win == 1)
-				player = entities[3].name;
-			else
-				player = entities[4].name;
-			if (player == yourName)
-				scene[1].win = 1;
-			else
-				scene[1].win = 2;
-			flag.STOP = true
-			ws.send(
-				JSON.stringify({
-					'type' : 'game.clear',
-					'data' : {
-						'winner' : player,
-					}
-				})
-			)
-		}
-	}
+  function checkBoundary() {
+    let win;
+    if (entities[2].ball.X < entities[3].bar.X) {
+      entities[2].serve += 1;
+      entities[2].init();
+      entities[1].changeScore(1);
+    }
+    if (entities[2].ball.X > entities[4].bar.X + entities[4].bar.WIDTH) {
+      entities[2].serve += 1;
+      entities[2].init();
+      entities[1].changeScore(0);
+    }
+    entities[1].checkDuce();
+    win = entities[1].checkWin();
+    if (win) {
+      let player;
+      if (win == 1) player = entities[3].name;
+      else player = entities[4].name;
+      if (player == yourName) scene[1].win = 1;
+      else scene[1].win = 2;
+      flag.STOP = true;
+      ws.send(
+        JSON.stringify({
+          type: "game.clear",
+          data: {
+            winner: player,
+          },
+        })
+      );
+    }
+  }
 
-	function update() {
-		updateBar()
-		for (const entity of entities) {
-			entity.update()
-		}
-		checkWallCollision()
-		checkBarCollision()
-		checkBoundary()
-	}
+  function update(time) {
+    updateBar();
+    for (const entity of entities) {
+      entity.update(time);
+    }
+    checkWallCollision();
+    checkBarCollision();
+    checkBoundary();
+  }
 
-	function sendBarInfo() {
-		let bar;
-		if (yourName == entities[3].name) {
-			bar = {"x" : entities[3].bar.X, "y" : entities[3].bar.Y}
-		} else {
-			bar = {"x" : entities[4].bar.X, "y" : entities[4].bar.Y}
-		}
-		ws.send(
-			JSON.stringify({
-				'type' : 'bar.info',
-				'data' : {
-					'bar' : {'x' : bar['x'], 'y' : bar['y']}
-				}
-			})
-		)
-	}
+  function sendBarInfo() {
+    let bar;
+    if (yourName == entities[3].name) {
+      bar = { x: entities[3].bar.X, y: entities[3].bar.Y };
+    } else {
+      bar = { x: entities[4].bar.X, y: entities[4].bar.Y };
+    }
+    ws.send(
+      JSON.stringify({
+        type: "bar.info",
+        data: {
+          bar: { x: bar["x"], y: bar["y"] },
+        },
+      })
+    );
+  }
 
-	function start(time) {
-		window.requestAnimationFrame(start);
-		secondsPassed = (time - previousTime) / 1000;
-		previousTime = time;
-		checkWebSocketMessage();
-		if (flag.START == false || flag.STOP == true) {
-			sceneDraw();
-		} else {
-			update();
-			playingDraw();
-			sendBarInfo();
-		}
-	}
+  function start(time) {
+    window.requestAnimationFrame(start);
+    secondsPassed = (time - previousTime) / 1000;
+    previousTime = time;
+    checkWebSocketMessage();
+    if (flag.START == false || flag.STOP == true) {
+      sceneDraw();
+    } else {
+      update(secondsPassed);
+      playingDraw();
+      sendBarInfo();
+    }
+  }
 
-	window.requestAnimationFrame(start)
-	document.addEventListener("keydown", keyDownHandler, false);
-	document.addEventListener("keyup", keyUpHandler, false);
+  window.requestAnimationFrame(start);
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
 
-	function keyDownHandler(event) {
-		let player;
-		if (yourName == entities[3].name)
-			player = entities[3]
-		else
-			player = entities[4]
-		if (event.keyCode == 87)
-			player.upPressed = true
-		else if (event.keyCode == 38)
-			player.upPressed = true
-		else if (event.keyCode == 83)
-			player.downPressed = true
-		else if (event.keyCode == 40)
-			player.downPressed = true
-	}
-	
-	function keyUpHandler(event) {
-		let player;
-		if (yourName == entities[3].name)
-			player = entities[3]
-		else
-			player = entities[4]
-		if (event.keyCode == 87)
-			player.upPressed = false
-		else if (event.keyCode == 38)
-			player.upPressed = false
-		else if (event.keyCode == 83)
-			player.downPressed = false
-		else if ( event.keyCode == 40)
-			player.downPressed = false
-	}
-})
+  function keyDownHandler(event) {
+    let player;
+    if (yourName == entities[3].name) player = entities[3];
+    else player = entities[4];
+    if (event.keyCode == 87) player.upPressed = true;
+    else if (event.keyCode == 38) player.upPressed = true;
+    else if (event.keyCode == 83) player.downPressed = true;
+    else if (event.keyCode == 40) player.downPressed = true;
+  }
+
+  function keyUpHandler(event) {
+    let player;
+    if (yourName == entities[3].name) player = entities[3];
+    else player = entities[4];
+    if (event.keyCode == 87) player.upPressed = false;
+    else if (event.keyCode == 38) player.upPressed = false;
+    else if (event.keyCode == 83) player.downPressed = false;
+    else if (event.keyCode == 40) player.downPressed = false;
+  }
+});
