b9b671351553e38107c242c7957464785d87a985
Namkyung Koo <united94@naver.com>
2024-04-30 18:23:19 +0900

    Add : disconnect() in game/matching.py

--- srcs/requirements/django/src/game/matching.py
+++ srcs/requirements/django/src/game/matching.py
@@ -64,7 +64,7 @@ class PongOneMatch(AsyncWebsocketConsumer):
                         matched = True
                         self.channel_layer.group_send(
                             self.group_name, {
-                                "type" : 'game.message',
+                                "type" : 'game_message',
                                 "data" : {
                                     "room_name" : self.group_name,
                                     "mode" : room.mode,
@@ -75,7 +75,8 @@ class PongOneMatch(AsyncWebsocketConsumer):
                             }
                         )
                         self.game_start()
-                        break
+                    else:
+                        self.try_matching(player)
             # "waiting"인 방이 없으면 새로운 방을 만든다
             if not matched:
                 self.group_name = f"pong_one_{player.name}"
@@ -94,10 +95,12 @@ class PongOneMatch(AsyncWebsocketConsumer):
             self.rating_differece += 200
             return False
         
-        
     @database_sync_to_async
     def get_player_rating(self, player_name):
-        return UserRecordPongGame.objects.get(user_name=player_name).rating
+        try:
+            return UserRecordPongGame.objects.get(user_name=player_name).rating
+        except UserRecordPongGame.DoesNotExist:
+            return 1000
     
     # 웹소켓에 보낼 메세지들을 처리한다
     async def game_message(self, event):
@@ -105,23 +108,27 @@ class PongOneMatch(AsyncWebsocketConsumer):
 
     # 연결을 종료
     async def disconnect(self, close_code):
-        if close_code == 1001:
-            if self.game_group_name:
-                self.channel_layer.group_discard(self.game_group_name, self.channel_name)
-            room = self.get_room()
-            if room != None:
-                delattr(self.RoomList, self.game_group_name)
-        else:
-            if self.game_group_name:
-                self.channel_layer.group_send(
-                    self.game_group_name, {
-                        'type' : 'game.message',
-                        'data' : {
-                            'mode' : 'abnormal.termintaion',
+        if self.group_name:
+            try:
+                room = await database_sync_to_async(GameRoom.objects.get)(room_name=self.group_name)
+                self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+                if room in game_queue["one"]:
+                    game_queue["one"].remove(room)
+
+                # 정상 종료가 아니면 abnormal termination 메세지를 보낸다
+                if close_code != 1000:
+                    await self.channel_layer.group_send(
+                        self.group_name, {
+                            'type' : 'game_message',
+                            'data' : {
+                                'status' : 'abnormal.termination',
+                            }
                         }
-                    }
-                )
-                self.channel_layer.group_discard(self.game_group_name, self.channel_name)
+                    )
+                await database_sync_to_async(room.delete)()
+            except GameRoom.DoesNotExist:
+                pass
 
     # 웹소켓에서 받는 메세지를 처리
     async def receive(self, text_data):
@@ -158,41 +165,6 @@ class PongOneMatch(AsyncWebsocketConsumer):
         # 게임이 전부 끝나면 종료한다
         if msg_type == "game.clear":
             self.disconnect(1001)
-
-    # 매칭 시도
-    async def join_matching(self):
-        count = await self.get_room_cnt()
-        name = await self.get_room_name()
-        # 방이 하나도 안 만들어졌거나 전부 게임중이면 방을 새로 판다. 방이 있으면 해당 방에 참여
-        if count == 0 or name == "not":
-            await self.create_room()
-            await self.channel_layer.group_add(self.game_group_name, self.channel_name)
-            await self.channel_layer.group_discard("game_queue", self.channel_name)
-        else:
-            if name != "not":
-                self.game_group_name = name
-                await self.channel_layer.group_add(self.game_group_name, self.channel_name)
-                await self.channel_layer.group_discard("game_queue", self.channel_name)
-                await self.set_player1()
-                player = await self.get_player()
-                # if await self.check_rating():
-                await self.channel_layer.group_send(
-                    self.game_group_name, {
-                        "type" : 'game.message',
-                        "data" : {
-                            "mode" : "set.game",
-                            "player0" : player["player0"],
-                            "player1" : player["player1"],
-                            "group" : self.game_group_name,
-                        }
-                    }
-                )
-            else:
-                await self.join_matching()
-
-    # 본인이 속한 방을 불러온다. 없으면 None
-    async def get_room(self):
-        return getattr(self.RoomList, self.game_group_name, None)
     
     # 사용자에게 받은 탁구채 위치 정보를 최신화
     async def update_bar_info(self):
@@ -214,57 +186,6 @@ class PongOneMatch(AsyncWebsocketConsumer):
                 }
             }
         )
-
-
-    # database를 이용하여 사람 수를 세거나 할 때 사용
-    @database_sync_to_async
-    def db_cnt(self):
-        is_room = PracticeGameRoom.objects.get(name=self.game_group_name)
-        cnt = is_room.cnt
-        cnt += 1
-        if cnt < 2:
-            is_room.cnt = cnt
-        else:
-            is_room.cnt = 0
-        is_room.save()
-        return cnt
-
-    # 게임방이 얼마나 있는 지 확인할 때 사용
-    @database_sync_to_async
-    def get_room_cnt(self):
-        return PracticeGameRoom.objects.count()
-    
-    # 방을 만든다. 방 이름은 연습 게임이기에 test + 랜덤 생성 아스키 코드 6자리
-    @database_sync_to_async
-    def create_room(self):
-        self.game_group_name = "test" + random_key(6)
-        is_room = PracticeGameRoom(name=self.game_group_name, player0=self.channel_name)
-        is_room.save()
-    
-    # 들어갈 수 있는 방을 검색하고 들어간 방의 이름을 알려준다
-    @database_sync_to_async
-    def get_room_name(self):
-        room_all = PracticeGameRoom.objects.all()
-        for room in room_all:
-            if room.status == "waiting":
-                room.player1 = self.channel_name
-                room.status = "full"
-                room.save()
-                return room.name
-        return "not"
-    
-    # 게임방에 두번째 사용자 등록
-    @database_sync_to_async
-    def set_player1(self):
-        is_room = PracticeGameRoom.objects.get(name=self.game_group_name)
-        is_room.player1 = self.channel_name
-        is_room.save()
-    
-    # 게임방에 속한 플레이어들을 불러온다
-    @database_sync_to_async
-    def get_player(self):
-        is_room = PracticeGameRoom.objects.get(name=self.game_group_name)
-        return {"player0": is_room.player0, "player1": is_room.player1}
 
 # class PongTwoMatch(AsyncWebsocketConsumer):
 
