7802311523c6bcb957f1d62baedde89b122e0dcc
Namkyung Koo <united94@naver.com>
2024-05-02 16:44:48 +0900

    Delete : practice

--- srcs/requirements/django/src/game/routing.py
+++ srcs/requirements/django/src/game/routing.py
@@ -4,7 +4,7 @@ from game.consumers import practiceFightingConsumers, fightConsumers, PongConsum
 
 websocket_urlpatterns = [
 	re_path("ws/game/pongonebyone", PongConsumers.PongOneConsumers.as_asgi()),
-	re_path("ws/game/pongtwobytwo", PongConsumers.PongTwoConsumers.as_asgi()),
-	re_path("ws/game/pongtournament", PongConsumers.PongTournamentConsumers.as_asgi()),
+	# re_path("ws/game/pongtwobytwo", PongConsumers.PongTwoConsumers.as_asgi()),
+	# re_path("ws/game/pongtournament", PongConsumers.PongTournamentConsumers.as_asgi()),
 	path("ws/game/fighting", fightConsumers.fightingConsumers.as_asgi()),
 ]
